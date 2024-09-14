1ff6051e4ed6cb3c36a9740b0c466eadc2ebb123
David Bousi <bousi.david@gmail.com>
2024-09-14 20:02:18 +0200

    change register

--- src/copy_register.rs
+++ src/copy_register.rs
@@ -5,43 +5,17 @@ const MAX_NAMED_REGISTERS: usize = 26;
 const MAX_NUMBERED_REGISTERS: usize = 10;
 
 pub struct CopyRegister {
-    named_registers: HashMap<char, CopyObject>,
+    named_registers: HashMap<char, Vec<char>>,
     /// `VecDeque` is used instead of a Vec to avoid having to use indexing by numbers not matching
     /// the register (e.g. storing register 0 at index 9, due to the pushing)
-    numbered_register: VecDeque<CopyObject>,
-}
-
-pub enum CopyObject {
-    // for Yanking Text
-    Text(String),
-    // Later macro implementation will use ActionSequences
-    ActionSequence(ActionSequence),
-}
-impl CopyObject {
-    fn replace(&mut self, obj: Self) {
-        *self = obj;
-    }
-    pub const fn as_text(&self) -> Option<&String> {
-        if let Self::Text(text) = self {
-            Some(text)
-        } else {
-            None
-        }
-    }
-    const fn as_macro(&self) -> Option<&ActionSequence> {
-        if let Self::ActionSequence(seq) = self {
-            Some(seq)
-        } else {
-            None
-        }
-    }
+    numbered_register: VecDeque<Vec<char>>,
 }
 
 pub struct ActionSequence;
 impl Default for CopyRegister {
     fn default() -> Self {
         let mut numbered_register = VecDeque::with_capacity(MAX_NUMBERED_REGISTERS);
-        numbered_register.push_front(CopyObject::Text(String::new()));
+        numbered_register.push_front(vec![]);
         Self {
             numbered_register,
             named_registers: HashMap::with_capacity(MAX_NAMED_REGISTERS),
@@ -50,8 +24,8 @@ impl Default for CopyRegister {
 }
 
 impl CopyRegister {
-    pub fn yank(&mut self, text: impl Into<String>, named: Option<char>) -> Result<()> {
-        let text = CopyObject::Text(text.into());
+    pub fn yank(&mut self, text: impl Into<Vec<char>>, named: Option<char>) -> Result<()> {
+        let mut text = text.into();
 
         if let Some(reg) = named {
             if !reg.is_alphabetic() || !reg.is_ascii_lowercase() {
@@ -59,27 +33,27 @@ impl CopyRegister {
             }
             self.named_registers.insert(reg, text);
         } else {
-            self.unnamed_register_mut().replace(text);
+            let unnamed = self.unnamed_register_mut();
+            std::mem::swap(unnamed, &mut text);
         }
         Ok(())
     }
     /// Grants access to what is simply the zeroth of the unnamed registers
-    fn unnamed_register(&self) -> &CopyObject {
+    fn unnamed_register(&self) -> &Vec<char> {
         &self.numbered_register[0]
     }
     /// Grants mutable access to what is simply the zeroth of the unnamed registers
-    fn unnamed_register_mut(&mut self) -> &mut CopyObject {
+    fn unnamed_register_mut(&mut self) -> &mut Vec<char> {
         &mut self.numbered_register[0]
     }
-    pub fn get_from_register(&self, named: Option<char>) -> Result<&CopyObject> {
+    pub fn get_from_register(&self, named: Option<char>) -> Result<&Vec<char>> {
         named.map_or_else(
             || Ok(self.unnamed_register()),
             |reg| self.named_registers.get(&reg).ok_or(Error::PatternNotFound),
         )
     }
-    pub fn push_into_numbered_registers(&mut self, text: impl Into<String>) {
-        self.numbered_register
-            .insert(1, CopyObject::Text(text.into()));
+    pub fn push_into_numbered_registers(&mut self, text: impl Into<Vec<char>>) {
+        self.numbered_register.insert(1, text.into());
         if self.numbered_register.len() > MAX_NUMBERED_REGISTERS {
             self.numbered_register.pop_back();
         }
