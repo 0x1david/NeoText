515c7c69398881ccde14c1351d6c4dc3cdd2ea3d
David Bousi <bousi.david@gmail.com>
2024-09-25 12:50:24 +0200

    fix

--- src/lsp/client.rs
+++ src/lsp/client.rs
@@ -1,4 +1,5 @@
-use super::parser::initialize_params
+use super::data::initialize_params;
+use crate::Result;
 pub struct LSPClient {}
 
 impl LSPClient {
@@ -9,10 +10,18 @@ impl LSPClient {
     // but it might not handle the workspace/symbol request.
     // Similarly, a development tool announces its ability to provide about to save notifications before a document is saved,
     // so that a server can compute textual edits to format the edited document before it is saved.
-    fn announce_capabilities() {}
+    fn announce_capabilities() -> Result<()> {
+        todo!()
+    }
     // https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#initialize
-    fn initialize() {}
-    fn send_request(self.dbg!) -> Result<()> {}
-    fn send_notification() {}
+    fn initialize() {
+        todo!()
+    }
+    fn send_request() -> Result<()> {
+        todo!()
+    }
+    fn send_notification() -> Result<()> {
+        todo!()
+    }
 }
 enum LSPAction {}
