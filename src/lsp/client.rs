b23b2f83766c493970ea2bb403cdea2bc1dc2f65
David Bousi <bousi.david@gmail.com>
2024-10-01 15:30:19 +0200

    SUQASHME: More prog

--- src/lsp/client.rs
+++ src/lsp/client.rs
@@ -1,19 +1,32 @@
+use std::path::PathBuf;
+
 use super::data::{initialize_params, Body};
 use crate::Result;
 use tokio::io::AsyncReadExt;
+use tokio::process::{ChildStdin, ChildStdout};
 use tokio::sync::mpsc;
 pub struct LSPClient {
     sender: mpsc::Sender<Body>,
     reader: tokio::io::BufReader<tokio::io::Stdin>,
+    server_stdin: ChildStdin,
+    server_stdout: ChildStdout,
 }
 
 impl LSPClient {
-    pub async fn new(channel: mpsc::Sender<Body>) -> Result<Self> {
+    pub async fn new(
+        channel: mpsc::Sender<Body>,
+        ext: FileType,
+        file_path: &PathBuf,
+    ) -> Result<Self> {
+        let (server_stdin, server_stdout) = Self::start_lsp_server(ext, file_path).await?;
+
         let stdin = tokio::io::stdin();
         let reader = tokio::io::BufReader::new(stdin);
         let client = Self {
             reader,
             sender: channel,
+            server_stdin,
+            server_stdout,
         };
 
         client.initialize().await?;
@@ -49,7 +62,46 @@ impl LSPClient {
             let mut parser = super::parser::LspParser::new(&msg);
             let content = parser.parse();
             let content = content.unwrap();
-            self.sender.send(content.body);
+            self.sender.send(content.body).await.unwrap();
         }
     }
+
+    async fn start_lsp_server(
+        ext: FileType,
+        file_path: &PathBuf,
+    ) -> Result<(ChildStdin, ChildStdout)> {
+        match ext {
+            FileType::Rust => start_rust_lsp(file_path).await,
+            _ => start_rust_lsp(file_path).await,
+        }
+    }
+}
+
+pub enum FileType {
+    Rust,
+    Python,
+    Unknown,
+}
+
+impl From<&str> for FileType {
+    fn from(value: &str) -> Self {
+        match value {
+            "rs" => FileType::Rust,
+            "py" => FileType::Python,
+            _ => FileType::Unknown,
+        }
+    }
+}
+
+async fn start_rust_lsp(file_path: &PathBuf) -> Result<(ChildStdin, ChildStdout)> {
+    let mut ra = tokio::process::Command::new("rust-analyzer")
+        .stdout(std::process::Stdio::piped())
+        .stdin(std::process::Stdio::piped())
+        .arg(file_path)
+        .spawn()
+        .expect("Failed starting Rust Analyzer");
+
+    let writer = ra.stdin.take().expect("Failed to get stdin");
+    let reader = ra.stdout.take().expect("Failed to get stdout");
+    Ok((writer, reader))
 }
